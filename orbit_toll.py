4e3fe3a15d049eae70c07245cca370f6bb827555
gongweijing <876887913@qq.com>
2023-11-15 20:16:35 +0800

    add formute 1

--- orbit_toll.py
+++ orbit_toll.py
@@ -1,4 +1,5 @@
 import numpy as np 
+import math
 
 # construct the constant
 G = 6.67*10**(-11)
@@ -6,3 +7,13 @@ M = 5.965*10**24
 R = 6.378*10**6
 
 # construct the data
+def cal_r_by_T(T:int)->int:
+    """
+    param:
+        T: the second of the T of satellite
+    return:
+        r: orbit radium
+    """
+    return math.pow((G*M*T^2)/(4*math.pi()),1.0/3)
+
+print(cal_r_by_T(1.5*3600))
